c20b091f1eb526c785329825edb6cf649a30d5c5
bodln <omer.sadikovic2001@gmail.com>
2024-09-25 21:04:38 +0200

    Adjust location history for quantity

--- InventoryManagment/DTOs/LocationHistoryDTO.cs
+++ InventoryManagment/DTOs/LocationHistoryDTO.cs
@@ -6,5 +6,6 @@ namespace InventoryManagment.DTOs
     {
         public Guid InventoryGUID { get; set; }
         public string LocationName { get; set; }
+        public int Quantity { get; set; } = 0;
     }
 }
---
+++ InventoryManagment/Migrations/20240925164247_redundant.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagment.Migrations
+{
+    /// <inheritdoc />
+    public partial class redundant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "LocationHistories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "LocationHistories");
+        }
+    }
+}
